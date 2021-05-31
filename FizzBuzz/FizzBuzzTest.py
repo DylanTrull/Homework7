638817d917e9dd7e918916e5d9548a7a8873f0e6
DylanTrull <trulld@oregonstate.edu>
2021-05-31 13:07:42 -0700

    Added FizzBuzz code, and first test works impelemented so it takes an input.

--- FizzBuzz/FizzBuzzTest.py
+++ FizzBuzz/FizzBuzzTest.py
@@ -6,7 +6,9 @@ class TestFizzBuzz(unittest.TestCase):
     def test_Fizz(self):
         for i in [3, 9, 12, 18, 21]:
             print("test", i)
-            self.assertMultiLineEqual(FizzBuzz(i), "fizz")
+            self.assertMultiLineEqual(FizzBuzz.FizzBuzz(i), "Fizz")
+
+
 
 
 if __name__ == '__main__':
