dccd396869384a19f8c7add93604a214e0321d0e
DylanTrull <trulld@oregonstate.edu>
2021-05-31 13:23:53 -0700

    Added new test, that tests a majority of possible leap years

--- Leap Year/LeapYearTest.py
+++ Leap Year/LeapYearTest.py
@@ -6,6 +6,11 @@ class TestLeapYear(unittest.TestCase):
     def test_leapyearSmall(self):
         self.assertEqual(LeapYear.LeapYear(4), True)
 
+    def test_leapYears(self):
+        for i in [4, 12, 24, 48, 100, 400]:
+            print("leap year ", i)
+        self.assertEqual(LeapYear.LeapYear(i), True)
+
 
 if __name__ == '__main__':
     unittest.main()
